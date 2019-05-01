2ddf163f32742b48f572468921ad935f18ededa2
SBC <iamsbc@gmail.com>
2019-05-01 20:35:08 +0800

    add StockDbModel by code first

---
+++ StockDbContext/Model/StockBase.cs
@@ -0,0 +1,13 @@
+﻿namespace StockDbContext.Model
+{
+    public class StockBase
+    {
+
+        public string StockNo { get; set; }
+
+        public string StockName { get; set; }
+
+        public bool Enabled { get; set; }
+
+    }
+}
