5d1cd9353cea7c3ea6106ce067e3b44f5dfb19b0
SBC <iamsbc@gmail.com>
2019-06-09 23:35:37 +0800

    init AddOrUpdateStockBases

--- StockMiner/Helper/DbHelper.cs
+++ StockMiner/Helper/DbHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using StockDbContext;
 using StockDbContext.Model;
 
 namespace StockMiner.Helper
@@ -9,13 +11,47 @@ namespace StockMiner.Helper
 
         internal static bool AddOrUpdateStockBases(List<StockBase> stockBases)
         {
+            using (var db = new StockDbModel())
+            {
+                AddStockBases(stockBases, db);
+                EditDisableStockBases(stockBases, db);
 
+                try
+                {
+                    db.SaveChanges();
+                    return true;
+                }
+                catch { }
+            }
             return false;
         }
 
         internal static List<StockBase> ReadStockBases()
         {
             throw new NotImplementedException();
+        }
+
+        private static void AddStockBases(List<StockBase> stockBases, StockDbModel db)
+        {
+            foreach (var stockBase in stockBases)
+            {
+                var target = db.StockBase.SingleOrDefault(s => s.No == stockBase.No);
+
+                if (target == null)
+                {
+                    db.StockBase.Add(stockBase);
+                }
+            }
+        }
+
+        private static void EditDisableStockBases(List<StockBase> stockBases, StockDbModel db)
+        {
+            var extra = db.StockBase.Except(stockBases);
+
+            foreach (var item in extra)
+            {
+                item.Enabled = false;
+            }
         }
 
     }
